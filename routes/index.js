236326c047f7b535615ab1c314b9b3c5b2e44abc
Jim Reinknecht <captainjimscharters@gmail.com>
2017-11-12 12:15:13 -0500

    added db search for items for main page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var Sequelize = require("sequelize");
+var db = require("../models");
+
 
 /* GET home page. */
 router.get('/', function(req, res) {
-    res.render('index', { title: 'Express' });
+    db.Product.findAll({
+        limit: 3,
+        order: [
+            [Sequelize.fn('RAND', '')]
+        ]
+    }).then(function(dbProducts) {
+        //res.json(dbProducts);
+        res.render("index", { product: dbProducts });
+    });
+    //res.render('index', { title: 'Express' });
 });
 
 router.get('/login', function(req, res) {
