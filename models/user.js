f1b1b9440615ab2d72c40a6ac36dea00380c57b2
William Rainaud <wrainaud@gmail.com>
2017-11-16 15:18:19 -0500

    Restructure Passport / Login

--- models/user.js
+++ models/user.js
@@ -1,15 +1,13 @@
+// Requiring bcrypt for password hashing. Using the bcrypt-nodejs version as the regular bcrypt module
+// sometimes causes errors on Windows machines
+var bcrypt = require("bcrypt-nodejs");
+// Creating our User model
 module.exports = function(sequelize, DataTypes) {
     var User = sequelize.define("User", {
-        username: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [5]
-            }
-        },
         email: {
             type: DataTypes.STRING,
             allowNull: false,
+            unique: true,
             validate: {
                 isEmail: true
             }
@@ -21,16 +19,20 @@ module.exports = function(sequelize, DataTypes) {
                 len: [5, 20]
             }
         }
-    }, {
-        underscored: true,
-        freezeTableName: true
     });
-
-
+    // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
+    User.prototype.validPassword = function (password) {
+        return bcrypt.compareSync(password, this.password);
+    };
+    // Hooks are automatic methods that run during various phases of the User Model lifecycle
+    // In this case, before a User is created, we will automatically hash their password
+    User.hook("beforeCreate", function (user) {
+        user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
+    });
 
     User.associate = function(models) {
         User.belongsToMany(models.Order, { through: 'Shipped', foreignKey: 'order_id' });
-    }
+    };
 
     return User;
 };
